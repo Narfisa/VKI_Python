5cbf09abc6af0fcec81f2fe07f559baa253e2b56
elensiya <valya.okulova@mail.ru>
2020-05-13 16:18:50 +0700

    add serializator

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import Second, first
+from rest_framework import routers
+from .views import BookViewSet
+router = routers.DefaultRouter()
+router.register('books', BookViewSet)
+
 
 urlpatterns = [
-    path('', first),
+    path('', include(router.urls)),
+    path('first', first),
     path('second', Second.as_view())
 ]
--- demo/views.py
+++ demo/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+from rest_framework import viewsets
+
+class BookViewSet(viewsets.ModelViewSet):
+	serializer_class = BookSerializer
+	queryset = Book.objects.all()
+
 
 def first(request):
     books = Book.objects.all()
